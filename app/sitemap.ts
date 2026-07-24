dca816460b539910a5740ae5301d0ebc0ca666c4
Amelia Stirton <ameliastirton@gmail.com>
2026-07-24 23:02:11 +1000

    updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -15,5 +15,13 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.3,
     },
+
+ {
+      url: `${site.siteUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.3,
+    },
+
   ];
 }
